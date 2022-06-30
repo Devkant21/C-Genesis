d920f7680ca8cdf840e661a8022f84f182bdca63
Devkant <devkant21@gmail.com>
2022-06-30 20:47:40 +0530

    sum

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, sum;
+    printf("Enter a \n");
+    scanf("%d", &a);
+    printf("Enter b \n");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("The sum is %d \n", sum);
+
+    return 0;
+}
